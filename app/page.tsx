3e59e6714a80d6dd10fe6635801115eabbe8d520
Ahmedkhang <160028006+Ahmedkhang@users.noreply.github.com>
2024-11-29 23:17:12 -0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,18 +4,18 @@ import Child from "@/components/child";
 
 export default function Home() {
   return (
-    <div className="flex w-screen">
+    <div className="flex w-screen bg-slate-100">
       <div className="w-screen text-xl text-gray-700 font-bold">
       <h1 className="text-center w-screen py-10 text-3xl">Freinds Details</h1>
 
 
       
-  <div className="flex m-10 justify-between px-10 bg-slate-300">
+  <div className="flex flex-col md:flex-row py-5 m-10 justify-between px-10 bg-slate-300">
      <Child name="Ahmed" age={20} favDish="Biryani" className="bg-red-400"/>
      <Child name="Hussain" age={21} favDish="Haleem"/>
      <Child name="Hammad" age={22} favDish="Beef Pulao"/>
   </div>
-  <div className="flex m-10 justify-between px-10 bg-red-200">
+  <div className="flex flex-col md:flex-row py-5 m-10 justify-between px-10 bg-red-200">
      <Child name="Talha" age={23} favDish="Haleem"/>
      <Child name="Hamza" age={21} favDish="Biryani"/>
      <Child name="Hasan" age={21}  favDish="Haleem"/>
